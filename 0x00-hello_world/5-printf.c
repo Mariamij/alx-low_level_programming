e20c50fec6449c1c905cf7c268218e09883851cb
Alarape <mariamalarape@yahoo.com>
2022-03-10 12:51:15 -0800

    My commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Prints text without error
+ *
+ *Return: 0 without errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
