5ea234f4dbb79173064b8606c75338eea933f328
Alarape <mariamalarape@yahoo.com>
2022-03-11 14:14:02 -0800

    My commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -35,7 +34,7 @@ int main(void)
 													}
 			else if ((j < 56) && (y == 57))
 			{
- 			y = 1 + ++j;
+				y = 1 + ++j;
 			}
 			else
 			{
