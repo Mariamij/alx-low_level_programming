b0aa41681e7c09c4430b83ff43029f8fc4ef7cc6
Alarape <mariamalarape@yahoo.com>
2022-03-15 09:31:26 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,14 @@
 #include "holberton.h"
+#include <unistd.h>
 
 /**
- * main- print Holberton
- * Return: 0
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char holberton[] = "Holberton";
-	int i;
-
-	for (i = 0; holberton[i] != '\0'; i++)
-	{
-		_putchar(holberton[i]);
-	}
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
