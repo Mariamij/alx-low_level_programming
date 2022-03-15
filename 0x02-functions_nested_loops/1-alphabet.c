ad459aa0bee29e31294732789403e320aa932d75
Alarape <mariamalarape@yahoo.com>
2022-03-15 07:33:02 -0700

    My commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet- lowercase
+ * Description: ASCII 97 - 122
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+
+	for (i = 97; i <= 122; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
