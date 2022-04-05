ecad792b69d58e4d56fe7377d9a87ddd997f5c4e
Mariamij <mariamalarape@yahoo.com>
2022-04-05 12:41:08 +0100

    My commit

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,35 +1,34 @@
 #include "main.h"
-
 /**
- * _strstr - gets the length of a prefix substring
- *
- * @haystack: source string
- * @needle: substring
- *
- * Return: pointer to the beginning of the located substring
+ * _strstr - locate a substring
+ * @haystack:string
+ * @needle:string
+ * Return:string
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int a, b, x;
+int x;
 
-	a = 0;
-	x = 0;
+	if (*needle == 0)
+		return (haystack);
 
-	while (haystack[a] != '\0')
+	while (*haystack)
 	{
-		b = 0;
-		while (needle[b + x] != '\0' && haystack[a + x] != '\0')
-			&&needle[b + x] == haystack[a + x])
-			{
-				if (haystack[a + x] != needle[b + x])
-					break;
-				x++;
-			}
-			if (needle[b + x] == '\0')
-				return (&haystack[a]);
-			b++;
-			a++;
+		x = 0;
+
+		if (haystack[x] == needle[x])
+		{
+			do {
+				if (needle[x + 1] == '\0')
+					return (haystack);
+
+					x++;
+
+			} while (haystack[x] == needle[x]);
+		}
+
+		haystack++;
 	}
-	return (0);
+
+	return ('\0');
 }
