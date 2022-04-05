763b534835cb50d670d0df88d91dfc1d9fb17d38
Mariamij <mariamalarape@yahoo.com>
2022-04-05 11:43:35 +0100

    Create the static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: source string
+ * @c: character to be located
+ *
+ *Return: pointer to c or NULL if c is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+		while (*s != '0')
+		{
+			if (*s == c)
+				return (s);
+			else if (*(s + 1) == c)
+				return (s + 1);
+			s++;
+		}
+
+		return (s + 1);
+}
