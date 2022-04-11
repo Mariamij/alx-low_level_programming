1e0d272f801334cb9e8de80011b4496b6a22c501
Mariamij <mariamalarape@yahoo.com>
2022-04-12 00:01:16 +0100

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,32 +1,81 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * new_dog - new dog
- * @name: name's dog
- * @age: age's dog
- * @owner: owner's dog
- * Return: newdog
- */
-
+ * new_dog - create new data structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Return: Pointer to new dog
+ **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
+	char *cpname, *cpowner;
+	int lenname, lenowner;
 
-	int i = 0, j = 0, k;
-	dog_t *doge;
-
-	whilee[k];
-	doge->age = age;
-	doge->owner = malloc(j * sizeof(doge->owner));
-	if (doge->owner == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	lenname = _strlen(name);
+	lenowner = _strlen(owner);
+	cpname = malloc(sizeof(char) * lenname + 1);
+	if (cpname == NULL)
 	{
-		free(doge->owner);
-		free(doge->name);
-		free(doge);
+		free(cpname);
+		free(new_dog);
 		return (NULL);
 	}
-	for (k = 0; k <= j; k++)
-		doge->owner[k] = owner[k];
-	return (doge);
+	cpname = _strcpy(cpname, name);
+	cpowner = malloc(sizeof(char) * lenowner + 1);
+	if (cpowner == NULL)
+	{
+		free(cpowner);
+		free(cpname);
+		free(new_dog);
+		return (NULL);
+	}
+	cpowner = _strcpy(cpowner, owner);
+	new_dog->name = cpname;
+	new_dog->age = age;
+	new_dog->owner = cpowner;
+	return (new_dog);
+}
+ /**
+ * _strlen - determinates the lenght of a string
+ * @s: pointer to string
+ * Return: the length
+ */
+int _strlen(char *s);
+{
+	int a;
+	int len;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		len++;
+	}
+	return (a);
+}
+ /**
+ * _strcpy - copies a pointed string
+ * @dest: pointer to the destine string
+ * @src: pointer to the source strin
+ * Return: the adress of the destiny string
+ */
+char *_strcpy(char *dest, char *src);
+{
+	int a;
+
+	a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
 }
